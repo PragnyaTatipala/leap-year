2f56d572d188172adba69edad5318738c3a630ee
PragnyaTatipala <31783056+PragnyaTatipala@users.noreply.github.com>
2019-06-10 12:49:23 -0400

    first

---
+++ leapYear/src/leapYear/LeapYear.java
@@ -0,0 +1,14 @@
+package leapYear;
+
+public class LeapYear {
+	public static void main(String[] args){
+        int year=2018;
+        if(year % 4==0){
+        System.out.println(year +"leap year");
+                }
+       else{
+      System.out.print(year +" not a leap year");
+             }
+        }
+
+}
